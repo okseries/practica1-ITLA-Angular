7bae6febe4f3d1eb8b5b9e2ec1dcad0afcb7a74a
okseries <yokseries211295@gmail.com>
2024-07-26 16:11:47 -0400

    first commit

---
+++ src/app/pages/ejecicios/ejecicios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejecicios',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejecicios.component.html',
+  styleUrl: './ejecicios.component.css'
+})
+export class EjeciciosComponent {
+
+}
